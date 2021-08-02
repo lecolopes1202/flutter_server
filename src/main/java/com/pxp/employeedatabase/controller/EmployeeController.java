c1f0bb0e91863e0d5abeea3618c6bbe8ce3b43df
lecolopes1202 <alexlopes1202@gmail.com>
2021-08-02 14:58:48 -0300

    sa

--- src/main/java/com/pxp/employeedatabase/controller/EmployeeController.java
+++ src/main/java/com/pxp/employeedatabase/controller/EmployeeController.java
@@ -25,7 +25,7 @@ public class EmployeeController {
 		return employeeService.getAllEmployees();
 	}
 	
-	@RequestMapping(value = "addemployee", method = RequestMethod.POST)
+	@RequestMapping(value = "addemployee", method = RequestMethod.POST,consumes = "application/json")
 	public String addEmployee(@RequestBody EmployeeEntity employee) {
 		return employeeService.addEmployee(employee);
 	}
